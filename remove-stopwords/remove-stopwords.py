6b56370d1a9e4d1e0bcdbe07546a0e65c358d8ed
Souvik Jana <souvikjana993@gmail.com>
2026-07-14 23:43:06 +0530

    Solve remove-stopwords

---
+++ remove-stopwords/remove-stopwords.py
@@ -0,0 +1,9 @@
+def remove_stopwords(tokens, stopwords):
+    """
+    Returns: list[str] - tokens with stopwords removed (preserve order)
+    """
+    j = []
+    for i in tokens:
+        if i not in stopwords:
+          j.append(i)  
+    return j
